2248bdcd2d5aacbf7b022c9b6a2590bdcc6a4087
S M Mohi Us Sunnat <suncha629@gmail.com>
2024-01-06 22:29:49 +0100

    Completed the starter project

--- source-code/starter-project/app/src/main/java/dev/sunnat629/githubpackage/MainActivity.kt
+++ source-code/starter-project/app/src/main/java/dev/sunnat629/githubpackage/MainActivity.kt
@@ -10,15 +10,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.sunnat629.githubpackage.ui.theme.GithubPackageTheme
 import dev.sunnat629.stringextension.StringExtension
 import dev.sunnat629.stringextension.StringExtension.encodeIntoBase64
@@ -47,31 +51,38 @@ fun RootView() {
                 horizontalAlignment = Alignment.Start,
                 verticalArrangement = Arrangement.Center
             ) {
-                StringExtensionDemo()
+                EmailValidatorUI()
             }
         }
     }
 }
 
 @Composable
-fun StringExtensionDemo() {
-    val userName = "Mohi Us Sunnat"
-    val password = "password".encodeIntoBase64()
-    val email = "mohiexample.com"
+fun EmailValidatorUI() {
+    val email = "example@email.com" // Replace with your email
+    val validationText = remember { mutableStateOf("Validate the emailId") }
+    val validationColor = remember { mutableStateOf(Color.Black) }
 
-    val color = if (StringExtension.isEmail(email)) Color.Black else Color.Red
-
-    Text(text = "User Name: $userName")
-    Spacer(modifier = Modifier.height(8.dp))
-
-    Text(text = "Password: $password")
-    Spacer(modifier = Modifier.height(8.dp))
-
-    Text(
-        text = "Email: $email",
-        color = color
-    )
-    Spacer(modifier = Modifier.height(8.dp))
+    Column {
+        Text(
+            text = email,
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(text = validationText.value, color = validationColor.value)
+        Spacer(modifier = Modifier.height(30.dp))
+        Button(onClick = {
+            if (StringExtension.isEmail(email)) {
+                validationText.value = "A valid emailId!!!"
+                validationColor.value = Color.Green
+            } else {
+                validationText.value = "An invalid emailId!!!"
+                validationColor.value = Color.Red
+            }
+        }) {
+            Text(text = "Validate")
+        }
+    }
 }
 
 @Preview(showBackground = true)
